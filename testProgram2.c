53c1ef42806234cde5578351a934fea7d7ea121f
Jeremy GUYET <jguyet@e1r9p3.42.fr>
2017-06-09 15:29:34 +0200

    On fera le system a deux ce soir

--- testProgram2.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int	main(int argc, char **argv)
-{
-	char bonjour[6] = "woodman\n";	
-	printf("%s", bonjour);
-}
-
--- testProgram3.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int	main(int argc, char **argv)
-{
-	char bonjour[8] = "woodman\n";	
-	printf("%s", bonjour);
-}
-
